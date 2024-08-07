8fa39cda3470b6a376e4691867347011de2d0b55
Henry Backman <henry.backman@camptocamp.com>
2024-08-07 10:07:29 +0200

    work on tests

--- product_attribute_merge/tests/test_product_attribute_merge.py
+++ product_attribute_merge/tests/test_product_attribute_merge.py
@@ -30,16 +30,30 @@ class TestProductAttributeMerge(SavepointCase):
         cls.car = cls.template_model.create({"name": "car"})
 
     @classmethod
+    def setUpAttributes(cls):
+        # create from_attribute with one color
+        cls.from_attribute = cls.env["product.attribute"].create({"name": "Old color"})
+        cls.from_attribute_value = cls.env["product.attribute.value"].create(
+            {"name": "Red", "attribute_id": cls.from_attribute.id}
+        )
+        # create into_attribute with one color
+        cls.into_attribute = cls.env["product.attribute"].create({"name": "New color"})
+        cls.into_attribute_value = cls.env["product.attribute.value"].create(
+            {"name": "Red", "attribute_id": cls.into_attribute.id}
+        )
+
+
+    @classmethod
     def setUpClassAttribute(cls):
         cls.attribute_model = cls.env["product.attribute"]
-        cls.value_model = cls.env["product.attribute.value"]
-        cls.attribute_line_model = cls.env["product.template.attribute.line"]
-        cls.attribute_value_model = cls.env["product.template.attribute.value"]
-        cls.attribute = cls.attribute_model.create({"name": "color"})
+        cls.attribute_value_model = cls.env["product.attribute.value"]
+        cls.template_attribute_line_model = cls.env["product.template.attribute.line"]
+        cls.template_attribute_value_model = cls.env["product.template.attribute.value"]
+        cls.color_attribute = cls.attribute_model.create({"name": "color"})
         cls.values = cls.env["product.attribute.value"]
         for color in ["red", "blue", "green", "black"]:
-            value = cls.value_model.create(
-                {"attribute_id": cls.attribute.id, "name": color}
+            value = cls.attribute_value_model.create(
+                {"attribute_id": cls.color_attribute.id, "name": color}
             )
             setattr(cls, color, value)
             cls.values |= value
@@ -47,17 +61,17 @@ class TestProductAttributeMerge(SavepointCase):
     @classmethod
     def setUpClassProduct(cls):
         cls.product_model = cls.env["product.product"]
-        cls.pen_attribute_line = cls.attribute_line_model.create(
+        cls.pen_attribute_line = cls.template_attribute_line_model.create(
             {
                 "product_tmpl_id": cls.pen.id,
-                "attribute_id": cls.attribute.id,
+                "attribute_id": cls.color_attribute.id,
                 "value_ids": [(6, 0, cls.values.ids)],
             }
         )
-        cls.car_attribute_line = cls.attribute_line_model.create(
+        cls.car_attribute_line = cls.template_attribute_line_model.create(
             {
                 "product_tmpl_id": cls.car.id,
-                "attribute_id": cls.attribute.id,
+                "attribute_id": cls.color_attribute.id,
                 "value_ids": [(6, 0, cls.values.ids)],
             }
         )
@@ -65,13 +79,13 @@ class TestProductAttributeMerge(SavepointCase):
 
     @classmethod
     def setUpClassCommon(cls):
-        cls.red_template_values = cls.attribute_value_model.search(
+        cls.red_template_values = cls.template_attribute_value_model.search(
             [("product_attribute_value_id", "=", cls.red.id)]
         )
         cls.red_products = cls.red_template_values.ptav_product_variant_ids
         cls.red_templates = cls.red_products.mapped("product_tmpl_id")
         # Probably a way to do better
-        pen_template_value = cls.attribute_value_model.search(
+        pen_template_value = cls.template_attribute_value_model.search(
             [
                 ("product_attribute_value_id", "=", cls.red.id),
                 ("attribute_line_id", "=", cls.pen_attribute_line.id),
@@ -81,7 +95,7 @@ class TestProductAttributeMerge(SavepointCase):
 
     @classmethod
     def _get_template_line_from_templates(cls, product_tmpls):
-        return cls.attribute_line_model.search(
+        return cls.template_attribute_line_model.search(
             [
                 "|",
                 ("active", "=", True),
@@ -99,25 +113,52 @@ class TestProductAttributeMerge(SavepointCase):
     #------------------------------------------
 
     def _get_wizard(self, product_attribute):
-        context = {"active_model": "product.attribute", "active_ids": product_attribute.ids}
-        return Form(self.wizard_model.with_context(context)).save()
+        context = {
+            "active_model": self.env["product.attribute"], 
+            "active_ids": product_attribute.ids
+        }
+        return self.env["wizard.product.attribute.merge"].with_context(context)
 
 
     #------------------------------------------
 
 
     def test_merge(self):
-        self.color_attribute = self.attribute
+        # create from_attribute with one color
+        from_attribute = self.color_attribute
 
-        self.colour_attribute = self.env.ref("product.product_attribute_2")
-        wiz_model = self.env["wizard.product.attribute.merge"].with_context(
-            active_model=self.env["product.attribute"],
-            active_ids=self.color_attribute.ids,
-        )
+        from_attribute = self.color_attribute
+
+        into_attribute = self.env.ref("product.product_attribute_2")
+        # wizard_model = self.env["wizard.product.attribute.merge"].with_context(
+        #     active_model=self.env["product.attribute"],
+        #     active_ids=from_attribute.ids,
+        # )
+        wizard_model = self._get_wizard(from_attribute)
         # wiz = wiz_model.create({"product_attribute_id": self.color_attribute.id})
-        with Form(wiz_model) as form:
-            self.assertEqual(form.product_attribute_id, self.color_attribute)
-            form.into_product_attribute_id = self.colour_attribute
+        with Form(wizard_model) as form:
+            # assert correct attribute is selected
+            self.assertTrue(form.product_attribute_id == from_attribute)
+
+            # select the into_attribute
+            form.into_product_attribute_id = into_attribute     
+            self.assertTrue(form.into_product_attribute_id == into_attribute)
+
+            # assert only one action
+            breakpoint()
+            self.assertTrue(len(form.attribute_values_merge_action_ids) == 1)
+
+            # check action is marked as merge
+            action_line = form.attribute_values_merge_action_ids[0]
+            self.assertTrue(action_line.attribute_value_action == "merge")
+
+            # apply merge (clicking VALIDATE button)
+            form.action_merge()
+
+        # assert the old attribute has been removed
+
+        # assert the into_attribute has the color attribute added
+
 
     def test_merge_one(self):
         """ """
@@ -184,26 +225,25 @@ class TestProductAttributeMerge(SavepointCase):
 
     def test_move_value_attribute_only_one_value_on_ptal(self):
         # Has White, Black
-        color= self.env.ref("product.product_attribute_2")
+        colour = self.env.ref("product.product_attribute_2")
         # Has Black, Pink, Orange
         colour = self.env.ref("product_attribute_merge.attribute_colour")
         # Remove the black from the ptal
-        __import__("pdb").set_trace()
         black = self.env.ref("product.product_attribute_value_4")
-        color.attribute_line_ids.value_ids = [(2, black.id, 0)]
+        colour.attribute_line_ids.value_ids = [(2, black.id, 0)]
         # Move White to colour
         value_to_move = self.env.ref("product.product_attribute_value_3")
         related_variant_before = self._get_value_related_variant(value_to_move)
         wizard = self._get_wizard(colour)
         wizard._move_attribute_value(value_to_move, colour)
-        self.assertTrue(value_to_move not in color.value_ids)
+        self.assertTrue(value_to_move not in colour.value_ids)
         self.assertTrue(value_to_move in colour.value_ids)
         # TODO check same products have the attribute
         related_variant_after = self._get_value_related_variant(value_to_move)
         self.assertEqual(related_variant_before, related_variant_after)
 
     def test_delete_value_unused(self):
-        color= self.env.ref("product.product_attribute_2")
+        colour = self.env.ref("product.product_attribute_2")
         colour = self.env.ref("product_attribute_merge.attribute_colour")
         value_to_delete = self.env.ref("product_attribute_merge.attribute_value_orange")
         self.assertTrue(value_to_delete in colour.value_ids)
